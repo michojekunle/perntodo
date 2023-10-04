911e30b5c2810ee362c190c2722041132f8d7fd3
AMD <90222287+michojekunle@users.noreply.github.com>
2023-10-04 11:19:16 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
 
-const pg = require('pg');
+const { Client } = require('pg');
 
-const Pool = pg.Pool;
-
-const pool = new Pool({
+const client = new Client({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    connectionString: process.env.DB_CONNECTION_STRING,
 });
 
-module.exports = pool;
+module.exports = client;
