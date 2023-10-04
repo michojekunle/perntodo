61c8a57c7d00bbc63b4a591bf479e22ec6a4dd3e
AMD <90222287+michojekunle@users.noreply.github.com>
2023-10-04 11:20:35 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,11 +3,6 @@ require('dotenv').config();
 const { Client } = require('pg');
 
 const client = new Client({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
     connectionString: process.env.DB_CONNECTION_STRING,
 });
 
